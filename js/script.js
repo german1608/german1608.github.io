12ce54c42f61d20b321b2ace1cf4456054675979
German Robayo Paz <germanrobayo33@gmail.com>
2017-07-02 23:33:20 -0400

    Update the main files

--- js/script.js
+++ js/script.js
@@ -1,40 +1,64 @@
 $(document).ready(function () {
   $(".button-collapse").sideNav();
-});
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+  $('.parallax').parallax();
+  $('.modal').modal();
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 500, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
       }
-    }
+  });
+
+  $('ul.side-nav a').click( function() {
+    $('.button-collapse').sideNav('hide');
+  });
+
+  $('#submit').click(function(e){
+    let $inputs = $("input, textarea").not('#submit');
+    // Checking for empty inputs
+    let exit = false;
+    $inputs.each(function(){
+      if (!$(this).val()) {
+        exit = true;
+      }
+    });
+
+    // Exit if there are empty inputs.
+    if (exit) return;
+
+    // Building the mail body
+    let body = `Hello German.\n${$($inputs[0]).val()} wants to contact you with the subject ${$($inputs[1]).val()}.\nContact him/her at ${$($inputs[2]).val()}`;
+    let link = `mailto:germanrobayo33@gmail.com?subject=${encodeURI('Contact Email')}&body=${encodeURI(body)}`;
+    // window.open('mailto:germanrobayo33@gmail.com');
+  });
 });
